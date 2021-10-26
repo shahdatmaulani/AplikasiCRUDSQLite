32e99c733db2ad0e2c58f0d8da00d09a81945a8f
shahdatmaulani <60951608+shahdatmaulani@users.noreply.github.com>
2021-10-27 06:16:35 +0700

    Initial commit

---
+++ app/src/main/java/com/example/shahdatmaulani/MainActivity.java
@@ -0,0 +1,118 @@
+package com.example.shahdatmaulani;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView text;
+    private EditText input, update;
+    private CRUD crud;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //inisialisasi component
+        text = findViewById(R.id.text);
+        input = findViewById(R.id.input);
+        update = findViewById(R.id.update);
+
+        Button btnCreate = findViewById(R.id.btnCreate);
+        Button btnUpdate = findViewById(R.id.btnUpdate);
+        Button btnDelete = findViewById(R.id.btnDelete);
+
+        crud = new CRUD(this);
+
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!input.getText().toString().isEmpty()) {
+                    boolean create = crud.createData(input.getText().toString());
+                    if (create) {
+                        sukses("Create");
+                    } else {
+                        gagal("Create");
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "Kolom Input Nama Tidak Boleh Kosong", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (validasi(false)) {
+                    boolean updates = crud.updateData(update.getText().toString(), input.getText().toString());
+                    if (updates) {
+                        sukses("Update");
+                    } else {
+                        gagal("Update");
+                    }
+                }
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (validasi(true)) {
+                    boolean delete = crud.deleteData(input.getText().toString());
+                    if (delete) {
+                        sukses("Delete");
+                    } else {
+                        gagal("Delete");
+                    }
+                }
+            }
+        });
+
+        viewData();
+    }
+
+    private boolean validasi(boolean isDelete) {
+        String txtInput = input.getText().toString();
+        String txtUpdateDelete = update.getText().toString();
+
+        //nilai input & update/delete tidak boleh kosong
+        if (txtInput.isEmpty()) {
+            Toast.makeText(this, "Kolom Input Nama Tidak Boleh Kosong", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if (!isDelete && txtUpdateDelete.isEmpty()) {
+            Toast.makeText(this, "Kolom Update Nama Tidak Boleh Kosong", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        //nilai input harus ada di database sebelumnya
+        if (!crud.getDataByInput(txtInput)) {
+            Toast.makeText(this, "Nama yang Anda Input Tidak Ditemukan Didatabase", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        return true;
+    }
+
+    private void sukses(String info) {
+        Toast.makeText(MainActivity.this, info + " Berhasil", Toast.LENGTH_LONG).show();
+        input.setText("");
+        update.setText("");
+        viewData();
+    }
+
+    private void gagal(String info) {
+        Toast.makeText(MainActivity.this, info + " Gagal", Toast.LENGTH_LONG).show();
+    }
+
+    private void viewData() {
+        text.setText(crud.getData() != null ? crud.getData() : "-");
+    }
+
+    }
